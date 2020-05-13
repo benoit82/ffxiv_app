5ad2d887d8bd5a72ed1cf1b09a254b7aa3305f8f
Benoit <benoit.dubus@outlook.fr>
2020-05-13 23:58:57 +0200

    add firebase fnc ; xivapi-js ; better character form

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,11 +1,13 @@
 import app from "firebase/app";
 import config from "./firebase-config";
 import "firebase/auth";
+import "firebase/firestore";
 
 class Firebase {
   constructor() {
     app.initializeApp(config);
     this.auth = app.auth();
+    this.db = app.firestore();
   }
 
   /**
@@ -30,6 +32,20 @@ class Firebase {
    * Deconnection
    */
   signOutUser = () => this.auth.signOut();
-}
 
+  /**
+   * Reset password
+   * @param {string} email
+   */
+  passwordReset = (email) => this.auth.sendPasswordResetEmail(email);
+
+  /**
+   * Get a document by reference
+   * @param {string} collection name of the collection (users...)
+   * @param {string} docReference reference to the document seeked (userid...)
+   * @returns {Promise<T>}
+   */
+  getDocument = async (collection, docReference) =>
+    await this.db.collection(collection).doc(docReference).get();
+}
 export default Firebase;
