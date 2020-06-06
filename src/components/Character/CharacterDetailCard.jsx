352664692a5fc39728569b877fe94f884b7e8637
Benoit <benoit.dubus@outlook.fr>
2020-06-06 15:26:18 +0200

    chr card clickable to edit

--- src/components/Character/CharacterDetailCard.jsx
+++ src/components/Character/CharacterDetailCard.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import Card from 'react-bootstrap/Card'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { DeleteBtn } from '../formElements'
 import { FirebaseContext } from '../firebase'
 
@@ -9,6 +9,8 @@ import JobListDisplay from '../../utils/jobListDisplay'
 import { styleRole } from '../../utils/styleRole'
 
 const CharacterDetailCard = ({ character }) => {
+
+    const history = useHistory()
 
     const firebase = useContext(FirebaseContext)
 
@@ -25,28 +27,30 @@ const CharacterDetailCard = ({ character }) => {
     }
 
     return (
-        <Card border="dark" style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={portrait} />
-            <Card.Body style={style}>
-                <Card.Title>{name}</Card.Title>
+        <Card border="dark" style={{ width: '18rem' }} >
+            <div onClick={() => history.push(`/chr/${_id}`)}>
+                <Card.Img variant="top" src={portrait} />
+                <Card.Body style={style}>
+                    <Card.Title>{name}</Card.Title>
+                </Card.Body>
+                {mainJob &&
+                    <div className="d-flex justify-content-around">
+                        <JobListDisplay job={mainJob} />{secondJob && <JobListDisplay job={secondJob} />}{thirdJob && <JobListDisplay job={thirdJob} />}
+                    </div>
+                }
+                {!mainJob &&
+                    <Card.Text className="d-flex justify-content-around">
+                        Jobs à définir (éditez-le !)
+                </Card.Text>
+                }
+            </div>
+            <Card.Footer className="d-flex justify-content-around">
                 <Card.Text><a
+                    className="btn btn-info"
                     href={`https://fr.finalfantasyxiv.com/lodestone/character/${id}`}
                     target={"_blanck"}
-                ><span className="badge badge-pill badge-info">lodestone</span></a>
+                >lodestone</a>
                 </Card.Text>
-            </Card.Body>
-            {mainJob &&
-                <div className="d-flex justify-content-around">
-                    <JobListDisplay job={mainJob} />{secondJob && <JobListDisplay job={secondJob} />}{thirdJob && <JobListDisplay job={thirdJob} />}
-                </div>
-            }
-            {!mainJob &&
-                <Card.Text className="d-flex justify-content-around">
-                    Jobs à définir (éditez-le !)
-                </Card.Text>
-            }
-            <Card.Footer className="d-flex justify-content-around">
-                <Link to={`/chr/${_id}`} className="btn btn-success"><i className="fas fa-edit"></i>éditer</Link>
                 <DeleteBtn handleClick={() => { handleDelete(character) }} />
             </Card.Footer>
         </Card>
