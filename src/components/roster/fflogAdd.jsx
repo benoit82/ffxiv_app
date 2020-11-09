7202d3adfa211179ba3f2a9de2407ea4a992ebce
Benoit <benoit.dubus@outlook.fr>
2020-11-09 20:47:52 +0100

    fflog add : import from fflogs

--- src/components/roster/fflogAdd.jsx
+++ src/components/roster/fflogAdd.jsx
@@ -13,6 +13,7 @@ import Axios from 'axios'
 
 function FFLogAdd({ roster, patchList, onFormSubmit }) {
     const [showCalendar, setShowCalendar] = useState(false)
+    const [loading, setLoading] = useState(false)
     const firebase = useContext(FirebaseContext)
     const { user } = useContext(UserApi)
 
@@ -62,80 +63,82 @@ function FFLogAdd({ roster, patchList, onFormSubmit }) {
     }
 
     const importFFLogsData = async () => {
+        setLoading(true)
         try {
             const lastUserLog = await (await Axios.get(`https://www.fflogs.com/v1/reports/user/${user.fflogsAccount.name}?api_key=${user.fflogsAccount.apiKey}`)).data.shift()
             formik.setFieldValue("title", lastUserLog.title)
             formik.setFieldValue("fflogurl", `https://www.fflogs.com/reports/${lastUserLog.id}`)
-            formik.setFieldValue("dateRaid", new Date(lastUserLog.start))
-            //formik.setValues()
+            fetchPatch(new Date(lastUserLog.start))
 
         } catch (error) {
             showInfoMessage("error", error.message)
+        } finally {
+            setLoading(false)
         }
     }
 
-    return (
-        <>
-            {user.fflogsAccount.name &&
-                <Button style={{ marginBottom: "1rem", width: "100%" }} variant="info" onClick={importFFLogsData} ><i className="fas fa-external-link-alt"></i>Importer le dernier log depuis FF-Logs</Button>
-            }
-            <Form onSubmit={formik.handleSubmit}>
-                <Form.Group controlId="title">
+    return (<>
+        {user.fflogsAccount.name &&
+            <Button style={{ marginBottom: "1rem", width: "100%" }} variant="info" onClick={importFFLogsData} >
+                <i className="fas fa-external-link-alt"></i>{loading ? "Chargement des données" : "Importer le dernier log depuis FF-Logs"}
+            </Button>
+        }
+        <Form onSubmit={formik.handleSubmit}>
+            <Form.Group controlId="title">
+                <Form.Control
+                    type="text"
+                    placeholder="titre du run"
+                    value={formik.values.title}
+                    onChange={formik.handleChange}
+                    isInvalid={formik.touched.fflogurl && formik.errors.title}
+                />
+                <Form.Control.Feedback type="invalid">{formik.errors.title}</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group controlId="fflogurl">
+                <InputGroup>
                     <Form.Control
                         type="text"
-                        placeholder="titre du run"
-                        value={formik.values.title}
+                        placeholder="https://www.fflogs.com/reports/...."
+                        value={formik.values.fflogurl}
                         onChange={formik.handleChange}
-                        isInvalid={formik.touched.fflogurl && formik.errors.title}
+                        isInvalid={formik.touched.fflogurl && formik.errors.fflogurl}
+                        required
                     />
-                    <Form.Control.Feedback type="invalid">{formik.errors.title}</Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group controlId="fflogurl">
-                    <InputGroup>
-                        <Form.Control
-                            type="text"
-                            placeholder="https://www.fflogs.com/reports/...."
-                            value={formik.values.fflogurl}
-                            onChange={formik.handleChange}
-                            isInvalid={formik.touched.fflogurl && formik.errors.fflogurl}
-                            required
-                        />
-                        {formik.validateForm && <InputGroup.Append>
-                            <InputGroup.Text>
-                                <a href={formik.values.fflogurl} target="_blank" rel="noopener noreferrer">
-                                    <i className="fas fa-external-link-alt"></i>vérifier
+                    {formik.values.fflogurl.match(/^https:\/\/([a-z]{2}|www)\.fflogs\.com\/reports\/([a-zA-Z0-9]{16,})$/gi) && <InputGroup.Append>
+                        <InputGroup.Text>
+                            <a href={formik.values.fflogurl} target="_blank" rel="noopener noreferrer">
+                                <i className="fas fa-external-link-alt"></i>vérifier
                                 </a>
-                            </InputGroup.Text>
-                        </InputGroup.Append>}
-                    </InputGroup>
-                    <Form.Control.Feedback type="invalid">{formik.errors.fflogurl}</Form.Control.Feedback>
-                </Form.Group>
+                        </InputGroup.Text>
+                    </InputGroup.Append>}
+                </InputGroup>
+                <Form.Control.Feedback type="invalid">{formik.errors.fflogurl}</Form.Control.Feedback>
+            </Form.Group>
 
-                <Form.Group controlId="patch">
-                    <Form.Control
-                        as="select"
-                        value={formik.values.patch}
-                        onChange={formik.handleChange}
-                        custom>
-                        {patchList.map(patch => <option key={patch.releaseDate}>{patch.name}</option>)}
-                    </Form.Control>
-                </Form.Group>
+            <Form.Group controlId="patch">
+                <Form.Control
+                    as="select"
+                    value={formik.values.patch}
+                    onChange={formik.handleChange}
+                    custom>
+                    {patchList.map(patch => <option key={patch.releaseDate}>{patch.name}</option>)}
+                </Form.Control>
+            </Form.Group>
 
-                <Form.Group controlId="dateRaid">
-                    <Button style={{ marginBottom: "1rem", width: "100%" }} variant="outline-primary" onClick={handleCalendarClick} ><i className="far fa-calendar"></i>Raid du : {formik.values.dateRaid.toLocaleDateString()}</Button>
-                    {showCalendar && <Calendar
-                        value={formik.values.dateRaid}
-                        minDate={FFXIV_ARR_RELEASE_DATE}
-                        maxDate={new Date()}
-                        onChange={(value) => fetchPatch(value)}
-                    />}
-                </Form.Group>
+            <Form.Group controlId="dateRaid">
+                <Button style={{ marginBottom: "1rem", width: "100%" }} variant="outline-primary" onClick={handleCalendarClick} ><i className="far fa-calendar"></i>Raid du : {formik.values.dateRaid.toLocaleDateString()}</Button>
+                {showCalendar && <Calendar
+                    value={formik.values.dateRaid}
+                    minDate={FFXIV_ARR_RELEASE_DATE}
+                    maxDate={new Date()}
+                    onChange={(value) => fetchPatch(value)}
+                />}
+            </Form.Group>
 
-                <SendBtn isDisabled={formik.isSubmitting} label="Envoyer le lien" />
-                {/* <pre>{JSON.stringify(formik.values, null, 2)}</pre> */}
-            </Form>
-        </>
-    )
+            <SendBtn isDisabled={formik.isSubmitting} label="Envoyer le lien" />
+            {/* <pre>{JSON.stringify(formik.values, null, 2)}</pre> */}
+        </Form>
+    </>)
 }
 
 export default FFLogAdd
