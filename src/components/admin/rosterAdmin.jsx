6543f66527a1480d0c416ee75ba5085d0a558291
Benoit <benoit.dubus@outlook.fr>
2020-11-11 15:03:41 +0100

    filter roster admin

--- src/components/admin/rosterAdmin.jsx
+++ src/components/admin/rosterAdmin.jsx
@@ -4,12 +4,29 @@ import { Roster } from "../../models";
 import { RostersTable, RosterEdit, RosterView } from "../roster";
 import { Switch, Route } from "react-router-dom";
 import { showInfoMessage } from "../../utils/globalFunctions";
+import { useFormik } from "formik";
+import { Form } from "react-bootstrap";
+import Select from "react-select";
+import { ALL } from "../../utils/consts";
 
 
 const RosterAdmin = () => {
     const firebase = useContext(FirebaseContext);
     const [rosters, setRosters] = useState([]);
+    const formik = useFormik({
+        initialValues: { roster: { value: ALL, label: ALL } }
+    })
 
+    const handleSelectChange = (selected) => {
+        const value = (selected) ? { label: selected.label, value: selected.value } : formik.initialValues.roster
+        formik.setFieldValue("roster", value)
+
+    }
+    const filteredRosters = (roster) => {
+        let copyRosters = rosters;
+        if (roster.value !== ALL) copyRosters = copyRosters.filter(r => r.value === roster.value)
+        return copyRosters;
+    }
     useEffect(() => {
         // load the roster
         const unsubcribe = firebase.db
@@ -26,13 +43,27 @@ const RosterAdmin = () => {
             );
 
         return () => unsubcribe();
-    }, [firebase]);
+    }, [firebase.db]);
 
     return (
-        <section style={{ display: "flex", flexDirection: "column" }}>
+        <section style={{ display: "flex", flexDirection: "column", minWidth: "800px" }}>
             <h2>Liste des rosters existants</h2>
             {rosters.length > 0 ? (
-                <RostersTable rosters={rosters} />
+                <>
+                    <Form>
+                        <Form.Group controlId="roster_id">
+                            <Select
+                                name="roster_id"
+                                options={[{ label: ALL, value: ALL }, ...rosters]}
+                                isClearable={true}
+                                value={formik.values.roster}
+                                clearValue={() => formik.setFieldValue("roster", formik.initialValues.roster)}
+                                onChange={handleSelectChange}
+                            />
+                        </Form.Group>
+                    </Form>
+                    <RostersTable rosters={filteredRosters(formik.values.roster)} />
+                </>
             ) : (
                     <p>aucun roster créer</p>
                 )}
