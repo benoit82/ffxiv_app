e42f4d91af9d55da96cf2af8cfe388cea5ab4853
Benoit <benoit.dubus@outlook.fr>
2020-12-02 16:15:00 +0100

    resetbtn disable option added

--- src/components/formElements/resetBtn.jsx
+++ src/components/formElements/resetBtn.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
 
-const ResetBtn = ({ label, handleReset }) => {
+const ResetBtn = ({ label, handleReset, isDisabled }) => {
   const labelBtn = label || 'réinitialiser'
+  const isDisabledBtn = isDisabled || false
   return (
-    <Button className='m-1' variant='secondary' type='reset' onClick={handleReset}>
+    <Button className='m-1' variant='secondary' type='reset' onClick={handleReset} disabled={isDisabledBtn}>
       <i className='fas fa-recycle' />{labelBtn}
     </Button>
   )
