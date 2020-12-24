caca6bd29b4d82860438fe70de7547cdcd03fbba
Benoit <benoit.dubus@outlook.fr>
2020-12-24 13:53:35 +0100

    regex rule update

--- src/components/roster/fflogAdd.jsx
+++ src/components/roster/fflogAdd.jsx
@@ -29,7 +29,7 @@ function FFLogAdd ({ roster, patchList, onFormSubmit }) {
   }
   const fflogValidationSchema = Yup.object().shape({
     title: Yup.string().trim().max(60, 'Titre trop long (max. 60 caractères)').notRequired(),
-    fflogurl: Yup.string().trim().required('La saisie est vide').matches(/^https:\/\/([a-z]{2}|www)\.fflogs\.com\/reports\/([a-zA-Z0-9]{16,})$/gi, "Ce n'est pas une URL FFLog valide")
+    fflogurl: Yup.string().trim().required('La saisie est vide').matches(/^https:\/\/([a-z]{2}|www)\.fflogs\.com\/reports\/([a-zA-Z0-9]{16,})\/?/gi, "Ce n'est pas une URL FFLog valide")
   })
   const formik = useFormik({
     initialValues,
